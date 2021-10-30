39e714e72fc637d768c62666ca1373b72ad51950
JeongHeon Woo <contact@wookingwoo.com>
2021-10-31 07:36:28 +0900

    Delete unusable sdk

--- app/src/main/java/com/wookingwoo/shoppingmalldemo/SignupActivity.java
+++ app/src/main/java/com/wookingwoo/shoppingmalldemo/SignupActivity.java
@@ -5,20 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.DataCollectionDefaultChange;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.ktx.Firebase;
 
 public class SignupActivity extends AppCompatActivity {
 
