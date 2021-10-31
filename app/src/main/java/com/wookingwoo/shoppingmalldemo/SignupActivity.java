553d879bc414a16689001337dd3f13505105fe11
JeongHeon Woo <contact@wookingwoo.com>
2021-10-31 10:02:09 +0900

    Add features: Validation of password.

--- app/src/main/java/com/wookingwoo/shoppingmalldemo/SignupActivity.java
+++ app/src/main/java/com/wookingwoo/shoppingmalldemo/SignupActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,11 +21,13 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.regex.Pattern;
+
 public class SignupActivity extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;
     private DatabaseReference mDatabaseRef;
-    private EditText mEtEmail, mEtPw;
+    private EditText mEtEmail, mEtPw, mETConfirmPW;
 
 
     @Override
@@ -38,45 +43,89 @@ public class SignupActivity extends AppCompatActivity {
 
         mEtEmail = findViewById(R.id.et_email);
         mEtPw = findViewById(R.id.et_pw);
+        mETConfirmPW = findViewById(R.id.et_confirmPW);
         Button mBtnSigup = findViewById(R.id.btn_register);
 
 
         mBtnSigup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 회원가입 시작
 
                 String strEmail = mEtEmail.getText().toString();
                 String strPw = mEtPw.getText().toString();
+                String strConfirmPW = mETConfirmPW.getText().toString();
 
 
-                mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPw).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        // 회원가입 성공할때
+                if (strEmail.length() == 0) {
+                    Toast.makeText(SignupActivity.this, "Input email", Toast.LENGTH_SHORT).show();
 
-                        if (task.isSuccessful()) {
-                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                } else if (!TextUtils.isEmpty(strEmail) && !(Patterns.EMAIL_ADDRESS.matcher(strEmail).matches())) {
+                    Toast.makeText(SignupActivity.this, "이메일 형식이 맞지 않습니다.", Toast.LENGTH_SHORT).show();
 
-                            UserAccount account = new UserAccount();
+                } else if (!Pattern.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&]).{8,15}.$", strPw)) {
+                    Toast.makeText(SignupActivity.this, "암호 규칙을 지켜주세요: (문자, 숫자, 특수문자를 모두 포함한 8~15자)", Toast.LENGTH_SHORT).show();
 
-                            account.setIdToken(firebaseUser.getUid());
-                            account.setEmailId(firebaseUser.getEmail());
-                            account.setPassword(strPw);
+                } else if (strConfirmPW.length() == 0) {
+                    Toast.makeText(SignupActivity.this, "Input Confirm Password", Toast.LENGTH_SHORT).show();
 
-                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+                } else if (!strPw.equals(strConfirmPW)) {
+                    Toast.makeText(SignupActivity.this, "비밀번호 확인이 다릅니다.", Toast.LENGTH_SHORT).show();
 
-                            Toast.makeText(SignupActivity.this, "Complete Sign up", Toast.LENGTH_SHORT).show();
+                } else {
+                    // 회원가입 시작
 
-                            Intent intent = new Intent(SignupActivity.this, SigninActivity.class);
-                            startActivity(intent);
 
-                        } else {
-                            Toast.makeText(SignupActivity.this, "Signup error", Toast.LENGTH_SHORT).show();
+                    mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPw).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            // 회원가입 성공할때
 
+                            if (task.isSuccessful()) {
+                                FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+
+                                UserAccount account = new UserAccount();
+
+                                account.setIdToken(firebaseUser.getUid());
+                                account.setEmailId(firebaseUser.getEmail());
+                                account.setPassword(strPw);
+
+                                mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+
+                                Toast.makeText(SignupActivity.this, "Complete Sign up", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(SignupActivity.this, SigninActivity.class);
+                                startActivity(intent);
+
+                            } else {
+
+                                if (task.getException() != null) {
+
+                                    String errorString = task.getException().toString();
+
+                                    if (errorString.equals("com.google.firebase.auth.FirebaseAuthInvalidCredentialsException: The email address is badly formatted.")) {
+                                        Toast.makeText(SignupActivity.this, "이메일 형식이 맞지 않습니다.", Toast.LENGTH_SHORT).show();
+
+                                    } else if (errorString.equals("com.google.firebase.auth.FirebaseAuthUserCollisionException: The email address is already in use by another account.")) {
+                                        Toast.makeText(SignupActivity.this, "이미 가입된 이메일입니다.", Toast.LENGTH_SHORT).show();
+
+                                    } else {
+                                        Toast.makeText(SignupActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+
+
+                                    Log.d("sign-up-error", errorString);
+
+
+                                } else {
+                                    Toast.makeText(SignupActivity.this, "Signup error", Toast.LENGTH_SHORT).show();
+                                }
+
+
+                            }
                         }
-                    }
-                });
+                    });
+
+                }
             }
         });
 
